c7dcdeccd90f59e997d37788d25b9e5760c14564
MizuiMiduki <105696149+MizuiMiduki@users.noreply.github.com>
2023-02-10 21:06:50 +0900

    R0.0.0.0.3

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-$(function() {
-    $("#year").text (new Date().getFullYear());});
+$(function () {
+    $("#year").text(new Date().getFullYear());
+});
+
+$(document).ready(function () {
+    $.ajax({
+        type: "get",
+        url: "sample.xml"
+    }).done(function (result) {
+        $(result).find("sample").each(function () {
+            $(".info-body").append('<h3>' + $(this).find('title').text() + '</h3>' + '<p>' + $(this).find('link').text() + '</p>');
+        });
+        console.log(result);
+    });
+});
